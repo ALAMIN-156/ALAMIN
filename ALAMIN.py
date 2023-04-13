32e672c80aea70c37bf7f6ed381e9bdf2f7b9a25
MrALAMIN156 <115931755+MrALAMIN156@users.noreply.github.com>
2023-04-14 01:50:30 +0600

    Update ALAMIN.py

--- ALAMIN.py
+++ ALAMIN.py
@@ -1,5 +1,5 @@
 import os, sys
-os.system('xdg-open https://github.com/MrALAMIN156/') 
+os.system('xdg-open https://github.com/MrALAMIN156/) 
 try:
     __import__("ALAMIN").bara()
 except Exception as e:
